9123f9974032dc547f6726b1647a02b88c62566b
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2022-10-10 14:03:41 +0200

    fix: implemente Shape interface for Star

--- star/star.go
+++ star/star.go
@@ -1,19 +1,18 @@
 package star
 
-func Star(s Perimeter) float64 {
-	return (s.x + s.y) * 5
+type Shape interface {
+	Perimeter() float64
+	Area() float64
 }
 
-func StarArea(a Area) float64 {
-	return (a.x * a.y) * 10
+type Star struct {
+	length float64
 }
 
-type Perimeter struct {
-	x float64
-	y float64
+func (s Star) Perimeter() float64 {
+	return (s.length + s.length) * 5
 }
 
-type Area struct {
-	x float64
-	y float64
+func (s Star) Area() float64 {
+	return (s.length * s.length) * 10
 }
--- star/star_test.go
+++ star/star_test.go
@@ -4,11 +4,11 @@ import "testing"
 
 func TestStar_Perimeter(t *testing.T) {
 	t.Run("perimeter 4 and 4", func(t *testing.T) {
-		r := Perimeter{
-			5, 5,
+		s := Star{
+			length: 5,
 		}
-		got := Star(r)
-		if got != 40 {
+		got := s.Perimeter()
+		if got != 50 {
 			t.Error("perimeter 5 and 5 is not 50")
 		}
 	})
@@ -16,12 +16,12 @@ func TestStar_Perimeter(t *testing.T) {
 
 func TestStar_Area(t *testing.T) {
 	t.Run("area 4 and 4 and 2", func(t *testing.T) {
-		a := Area{
-			2, 4.58257569495584,
+		s := Star{
+			length: 5,
 		}
-		got := StarArea(a)
-		if got != 91.6515138991168 {
-			t.Error("Area 2 and 4.58257569495584 and 10 is not 91.6515138991168")
+		got := s.Area()
+		if got != 250 {
+			t.Error("Area 2 and 4.58257569495584 and 10 is not 250")
 		}
 	})
 }
