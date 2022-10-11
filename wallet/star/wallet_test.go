c79d7d58055a50a874c9d48ae86400994b3a7668
Pierre Gorin <pierre.gorin@algosup.com>
2022-10-11 13:34:35 +0200

    Create wallet_test.go

---
+++ wallet/star/wallet_test.go
@@ -0,0 +1,14 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet()
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+	if got != want {
+		t.Error("got %d want %d got, want")
+	}
+}
